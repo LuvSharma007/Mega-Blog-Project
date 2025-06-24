76cd3feea92de810fc9b908048a9d3fd52edbb1d
UdayIge <udayige1696@gmail.com>
2025-06-24 14:40:48 +0530

    final fixes

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import authService from "../../appwrite/auth"
 import {logout}  from "../../store/authSlice"
+import { useNavigate } from 'react-router-dom'
 
 const LogoutBtn = () => {
     const dispatch = useDispatch();
